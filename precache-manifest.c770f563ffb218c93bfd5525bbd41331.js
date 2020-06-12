537196e80320eef3dc29de3bc8c88a3cf40d9a0c
shimele <bechembayamba@gmail.com>
2020-06-12 09:59:55 +0100

    Updates

---
+++ precache-manifest.c770f563ffb218c93bfd5525bbd41331.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ddd429e7f5684137fe684ddc81919c",
+    "url": "/Meme-Generator-React/index.html"
+  },
+  {
+    "revision": "73b3969faa446cf3dc29",
+    "url": "/Meme-Generator-React/static/css/main.58a92149.chunk.css"
+  },
+  {
+    "revision": "d74ad0460226f094e736",
+    "url": "/Meme-Generator-React/static/js/2.9f9350f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-Generator-React/static/js/2.9f9350f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b3969faa446cf3dc29",
+    "url": "/Meme-Generator-React/static/js/main.3a504479.chunk.js"
+  },
+  {
+    "revision": "8f11bc3c1f681bb064a6",
+    "url": "/Meme-Generator-React/static/js/runtime-main.b93fc22d.js"
+  }
+]);
